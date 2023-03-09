68d630d1c940231ee398c5fe44d2c04eae94af00
Geronimo Jauregui <geronimo.jauregui@isita.com.mx>
2023-03-09 15:11:49 -0600

    Add: Petición POST para CarritoCompras

---
+++ .NET/TiendaServicios/TiendaServicios.Api.CarritoCompra/Application/New.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.CarritoCompra.Models;
+using TiendaServicios.Api.CarritoCompra.Persistence;
+
+namespace TiendaServicios.Api.CarritoCompra.Application
+{
+    public class New
+    {
+        public class Ejecuta : IRequest
+        {
+            public DateTime? FechaCreacion { get; set; }
+            public List<string> ProductoLista { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly CarritoContext _context;
+
+            public Manejador(CarritoContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var carritoSesion = new CarritoSesion
+                {
+                    FechaCreacion = request.FechaCreacion
+                };
+
+                _context.CarritoSesion.Add(carritoSesion);
+                var value = await _context.SaveChangesAsync();
+                if(value == 0)
+                {
+                    throw new Exception("Errores en la inserción del carrito de compras");
+                }
+                int id = carritoSesion.CarritoSesionId;
+                foreach(var obj in request.ProductoLista)
+                {
+                    var carritoSesionDetalle = new CarritoSesionDetalle
+                    {
+                        FechaCreacion = DateTime.Now,
+                        CarritoSesionId = id,
+                        ProductoSeleccionado = obj
+                    };
+                    _context.CarritoSesionDetalles.Add(carritoSesionDetalle);
+                }
+                value = await _context.SaveChangesAsync();
+                if (value > 0)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se pudo insertar el detalle del carrito de compras");
+            }
+        }
+
+    }
+}
---
+++ .NET/TiendaServicios/TiendaServicios.Api.CarritoCompra/Controllers/CarritoComprasController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaServicios.Api.CarritoCompra.Application;
+
+namespace TiendaServicios.Api.CarritoCompra.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarritoComprasController : Controller
+    {
+        private readonly IMediator _mediator;
+        public CarritoComprasController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Crear(New.Ejecuta data)
+        {
+            return await _mediator.Send(data);
+        }
+    }
+}
