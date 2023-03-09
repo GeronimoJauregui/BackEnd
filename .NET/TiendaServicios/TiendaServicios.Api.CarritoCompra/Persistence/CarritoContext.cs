f0485dba2f39bb0462113a89d0af3faa899a442a
Geronimo Jauregui <geronimo.jauregui@isita.com.mx>
2023-03-09 13:06:25 -0600

    Add: Migración a base de datos MySQL de CarritoCompra

---
+++ .NET/TiendaServicios/TiendaServicios.Api.CarritoCompra/Persistence/CarritoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaServicios.Api.CarritoCompra.Models;
+
+namespace TiendaServicios.Api.CarritoCompra.Persistence
+{
+    public class CarritoContext : DbContext
+    {
+        public CarritoContext(DbContextOptions<CarritoContext> options) : base(options) { }
+
+        public DbSet<CarritoSesion> CarritoSesion { get; set; }
+        public DbSet<CarritoSesionDetalle> CarritoSesionDetalles { get; set; }
+    }
+}
